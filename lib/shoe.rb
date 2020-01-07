7709288f4e370eec60fac7d43c5cf14fef2560ba
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-07 21:22:02 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = ["Uggs", "Rainbow"]
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +13,19 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand
+    shoes.each do |brand|
+    BRANDS << brand.include("Uggs")
+  end
+  
+  def brand=(brand)
+    @brand = brand
+    shoes.each do |brand|
+    shoe::BRANDS == 3
+end
+
+end
 
 end
